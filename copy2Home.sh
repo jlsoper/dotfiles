20d6f83c8382622d63b2c580bb56ea499a9498e2
jlsoper <jlsoper@gmail.com>
2020-11-20 18:26:18 -0600

    permissions set 1st, copy 2 HOME 2nd

---
+++ copy2Home.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+/usr/bin/cp .X* $HOME
+/usr/bin/cp .b* $HOME
+/usr/bin/cp .d* $HOME
+/usr/bin/cp .n* $HOME
+/usr/bin/cp .v* $HOME
+/usr/bin/cp .x* $HOME
+
---
+++ perm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chmod 601 .dblogin
+
+chmod 645 .X* .b* .dir* .n* .vi* .x*
+
+
