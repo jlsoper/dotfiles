02880b12ba660f16899a0a9bd6283ed81c72cd68
James <soapman@zoho.com>
2023-06-20 02:21:52 -0500

    added ssh config

--- copy2Home.sh
+++ copy2Home.sh
@@ -9,3 +9,11 @@ cp  --preserve=mode .n* $HOME
 cp  --preserve=mode .v* $HOME
 cp  --preserve=mode .x* $HOME
 
+
+if [ ! -d ~/.ssh ]
+then
+  cp -R .ssh $HOME
+else
+  cp  --preserve=mode .ssh/config $HOME/.ssh
+fi
+
--- perm.sh
+++ perm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-chmod 600 .dblogin
+chmod 700 .ssh
+
+chmod 600 .dblogin .ssh/config
 
 chmod 644 .X* .b* .dir* .gitconfig .n* .vi*
 
